32272922a7f95d6037552a770227944e2ca0c146
Mehmet Yağcı <mehmetyagci53@gmail.com>
2023-08-06 17:55:21 +0300

    s3 part completed.

--- Cloud Fundamentals AWS Services for C# Developers/6.S3/Customers.Api/Program.cs
+++ Cloud Fundamentals AWS Services for C# Developers/6.S3/Customers.Api/Program.cs
@@ -1,4 +1,5 @@
 using Amazon.DynamoDBv2;
+using Amazon.S3;
 using Customers.Api.Repositories;
 using Customers.Api.Services;
 using Customers.Api.Validation;
@@ -26,6 +27,9 @@ builder.Services.AddSingleton<IAmazonDynamoDB, AmazonDynamoDBClient>();
 builder.Services.AddSingleton<ICustomerRepository, CustomerRepository>();
 builder.Services.AddSingleton<ICustomerService, CustomerService>();
 builder.Services.AddSingleton<IGitHubService, GitHubService>();
+
+builder.Services.AddSingleton<IAmazonS3, AmazonS3Client>();
+builder.Services.AddSingleton<ICustomerImageService, CustomerImageService>();
 
 builder.Services.AddHttpClient("GitHub", httpClient =>
 {
