6d3f2f9f3ae45378d9b0101a9127b38e4882e979
Mehmet Yağcı <mehmetyagci53@gmail.com>
2023-08-07 11:07:37 +0300

    aws secret manager completed.

---
+++ Cloud Fundamentals AWS Services for C# Developers/SecretsManager.App/Program.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+namespace SecretsManager.App {
+    internal class Program {
+        static async Task Main(string[] args) {
+
+            var secretsManagerClient = new AmazonSecretsManagerClient();
+
+            var listSecretVersionsRequest = new ListSecretVersionIdsRequest {
+                SecretId = "ApiKey",
+                IncludeDeprecated = true
+            };
+
+            var versionResponse = await secretsManagerClient.ListSecretVersionIdsAsync(listSecretVersionsRequest);
+
+            var request = new GetSecretValueRequest {
+                SecretId = "ApiKey",
+                VersionId = "600eff40-5fc5-4bdb-9e75-d2de1f7d0a81"
+                //VersionStage = "AWSCURRENT"
+            };
+
+            var response = await secretsManagerClient.GetSecretValueAsync(request);
+
+            Console.WriteLine(response.SecretString);
+
+            var describeSecretRequest = new DescribeSecretRequest {
+                SecretId = "ApiKey"
+            };
+
+            var describeResposne = await secretsManagerClient.DescribeSecretAsync(describeSecretRequest);
+
+            Console.WriteLine(describeResposne.ToString());
+        }
+    }
+}
